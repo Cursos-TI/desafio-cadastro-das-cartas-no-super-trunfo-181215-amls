66c133002c2eb9472fde6d2748ce6dd35d040aec
181215-amls <alllansaaantos@gmail.com>
2025-01-29 11:32:15 -0300

    Update CartasSuperTrunfo.c
    calcular média

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -1,22 +1,22 @@
 #include <stdio.h>
 
-// Desafio Super Trunfo - Países
-// Tema 1 - Cadastro das Cartas
-// Este código inicial serve como base para o desenvolvimento do sistema de cadastro de cartas de cidades.
-// Siga os comentários para implementar cada parte do desafio.
-//Teste larissa
+int main (){
+    int nota1, nota2, nota3;
+int média;
 
-int main() {
-    // Sugestão: Defina variáveis separadas para cada atributo da cidade.
-    // Exemplos de atributos: código da cidade, nome, população, área, PIB, número de pontos turísticos.
-    
-    // Cadastro das Cartas:
-    // Sugestão: Utilize a função scanf para capturar as entradas do usuário para cada atributo.
-    // Solicite ao usuário que insira as informações de cada cidade, como o código, nome, população, área, etc.
-    
-    // Exibição dos Dados das Cartas:
-    // Sugestão: Utilize a função printf para exibir as informações das cartas cadastradas de forma clara e organizada.
-    // Exiba os valores inseridos para cada atributo da cidade, um por linha.
+printf ("***programa de cálculo de média***\n");
 
-    return 0;
-}
+printf("digite a sua primeira nota: \n");
+scanf ("%d", &nota1);
+
+printf("digite a sua segunda nota: \n");
+scanf ("%d", &nota2);
+
+printf("digite a sua terceira nota: \n");
+scanf ("%d", &nota3);
+
+média = ( nota1 + nota2 + nota3) /3;
+
+printf("A média é: %d", média);
+
+return 0;
